2e0602fa8115fd9c627ef41ff286a727b0a4ba79
babaorere <baba.orere@gmail.com>
2020-10-05 18:46:42 -0300

    La pagina de registro funciona, y agrega a la base datos

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash
+from werkzeug.utils import redirect
 
 dbdir = "sqlite:///" + os.path.abspath(os.getcwd()) + "database.db"
 
@@ -12,40 +13,62 @@ app.config["SQLALCHEMY_DATABASE_URI"] = dbdir
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-class Users(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
+    nickname = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
 
 @app.route("/")
-def Inicio():
+def inicio():
     return render_template("home.html")
 
-@app.route("/Acercade")
-def Acerca():
-    return render_template("Acerca.html")
+@app.route("/Acerca_de/")
+def acerca_de():
+    return "acerca_de.html"
 
-@app.route("/Trilladoras")
-def Trilladoras():
-    return render_template("Trilladoras.html")
+#    return render_template("acerca_de.html")
 
-@app.route("/Foro")
-def Foro():
-    return render_template("Foro.html")
 
-@app.route("/registro", methods=["GET", "POST"])
+@app.route("/Trilladoras/")
+def trilladoras():
+    return "trilladoras.html"
+
+#    return render_template("trilladoras.html")
+
+
+@app.route("/Foro/")
+def foro():
+    return "foro.html"
+
+#    return render_template("Foro.html")
+
+@app.route("/Calculo_fr/")
+def calculo_fr():
+    return "calculo_fr.html"
+
+@app.route("/Informate/")
+def informate():
+    return "informate.html"
+
+@app.route("/Registro/", methods=["GET", "POST"])
 def registro():
     if request.method == "POST":
-        hashed_pw = generate_password_hash(request.form["password"], method="sha256")
-        new_user = Users(username=request.form["user"], password=hashed_pw)
+        # lee del campo nickname del form
+        nickname = request.form["nickname"]
+
+        # lee del campo password del form, y lo encripta
+        password = generate_password_hash(request.form["password"], method="sha256")
+
+        new_user = User(nickname=nickname, password=password)
         db.session.add(new_user)
         db.session.commit()
-        return "Su registro fue exitoso."
+        print("registro añadido")
+        return redirect(url_for('registro'))
     return render_template("registro.html")
 
-@app.route("/login")
-def loginn():
-    return render_template("login.html")
+@app.route("/Login/")
+def login():
+    return "login.html"
 
 if __name__ == "__main__":
     db.create_all()
